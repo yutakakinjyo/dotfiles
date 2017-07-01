f56d9df09618ec69c795531c4346ea8cd6ba7d04
yutakakinjyo <yutakakinjyo@gmail.com>
2017-07-02 00:04:08 +0900

    Add php-mode package to emacs
    1. package-refresh-contents
    2. package-install ( php-mode )

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -92,14 +92,17 @@
 (menu-bar-mode -1)
 
 (custom-set-faces
- ;; other faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(magit-diff-added ((((type tty)) (:foreground "green"))))
  '(magit-diff-added-highlight ((((type tty)) (:foreground "LimeGreen"))))
  '(magit-diff-context-highlight ((((type tty)) (:foreground "default"))))
  '(magit-diff-file-heading ((((type tty)) nil)))
  '(magit-diff-removed ((((type tty)) (:foreground "red"))))
  '(magit-diff-removed-highlight ((((type tty)) (:foreground "IndianRed"))))
-  '(magit-section-highlight ((((type tty)) nil))))
+ '(magit-section-highlight ((((type tty)) nil))))
 
 ;; http://www.helptouser.com/computer/576953-how-do-i-show-the-git-status-in-the-emacs-bottom-bar.html
 
@@ -139,3 +142,11 @@
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (php-mode yasnippet yaml-mode web-mode volatile-highlights use-package smex smartparens smart-mode-line-powerline-theme rinari puppet-mode projectile prodigy popwin pallet nyan-mode multiple-cursors markdown-mode magit irony idle-highlight-mode htmlize helm go-mode gitignore-mode gitconfig-mode flymake-yaml flymake-cppcheck flycheck-cask expand-region exec-path-from-shell drag-stuff csv-mode cmake-mode auto-complete anzu))))
